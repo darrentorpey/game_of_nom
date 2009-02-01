8bd023eea0d40f9261e330b14521776cfa9d9313
unknown <darren.torpey@gmail.com>
2009-02-01 14:18:27 -0500

    Added victory scree, state transitions, and fonts, fonts, fonts

---
+++ Connecting/GameOverSplash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Connecting
+{
+    class GameOverSplash : UIObject
+    {
+        static Texture2D s_MainTexture;
+
+        public GameOverSplash(Vector2 startLocation) : base(startLocation)
+        {
+            Location = startLocation;
+        }
+
+        protected override Texture2D getMainTexture()
+        {
+            return s_MainTexture;
+        }
+
+        public static void LoadContent(ContentManager aManager)
+        {
+            s_MainTexture = aManager.Load<Texture2D>("gui/ui_gameover_1");
+        }
+
+        public override void Draw(SpriteBatch aBatch, GameTime aTime)
+        {
+            aBatch.Draw(getMainTexture(), new Rectangle((int)(Location.X - (float)(s_MainTexture.Width / 2)), (int)Location.Y, getMainTexture().Width, getMainTexture().Height), Color.White);
+        }
+    }
+}
---
+++ Connecting/VictorySplash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Connecting
+{
+    class VictorySplash : UIObject
+    {
+        static Texture2D s_MainTexture;
+
+        public VictorySplash(Vector2 startLocation) : base(startLocation)
+        {
+            Location = startLocation;
+        }
+
+        protected override Texture2D getMainTexture()
+        {
+            return s_MainTexture;
+        }
+
+        public static void LoadContent(ContentManager aManager)
+        {
+            s_MainTexture = aManager.Load<Texture2D>("gui/ui_win_1");
+        }
+
+        public override void Draw(SpriteBatch aBatch, GameTime aTime)
+        {
+            aBatch.Draw(getMainTexture(), new Rectangle((int)(Location.X - (float)(s_MainTexture.Width / 2)), (int)Location.Y, getMainTexture().Width, getMainTexture().Height), Color.White);
+        }
+    }
+}
